cffd20230c1ba01a25a4536ef65161191927e177
Jim Hu <jim.hu.biobio@gmail.com>
2015-08-31 10:07:53 -0400

    Add DataTables extension to repository

---
+++ DataTables/DataTables.php
@@ -0,0 +1,199 @@
+<?php
+/*
+JQuery DataTables extension. Originally written by Daniel Renfro in 2012
+Updated to DataTables 1.10.8 by Jim Hu in August 2015
+*/
+// ============ check if we're inside MediaWiki =======================
+if ( !defined('MEDIAWIKI') ) {
+    echo <<<EOT
+To install this extension, you'll need to edit your LocalSettings.php with
+the appropriate configuration directives. Please see the README that comes
+with the software, or contact your system administrator with this message.
+EOT;
+    exit( 1 );
+}
+
+
+
+// ============= default global variables, constants, etc. =====================
+define( 'MW_DATATABLES_VERSION', '0.2.0' );
+define( 'DATATABLES_VERSION', '1.10.8' );
+
+// ============ credits =================================
+$wgExtensionCredits['other'][] = array(
+	'name' 			=> 'DataTables',
+	'description' 	=> 'Datatables jQuery library for MediaWiki',
+	'author' 		=> array(
+		'[mailto:bluecurio@gmail.com Daniel Renfro] (Mediawiki Extension)',
+		'[mailto:jimhu@tamu.edu Jim Hu]',
+		'Allan Jardine (Javascript library)'
+	),
+	'version' 		=> MW_DATATABLES_VERSION
+);
+
+
+// ============ hooks =================================
+$wgHooks['ResourceLoaderRegisterModules'][] = array( new DataTables, 'register' );
+
+
+// ============ Global Functions ============================================
+
+class DataTables {
+	/*
+	When updating this extension, use a global search and replace for the version number. PHP does not like using the constant DATATABLES_VERSION in a catenation for the array values.
+	*/
+	static $modules = array(
+	
+		'ext.datatables' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/media/js/jquery.dataTables.js'
+				),
+			'styles' => array(
+			    'DataTables-1.10.8/media/css/jquery.dataTables.css'
+			   // 'media/css/demo_table_jui.css'
+				),
+			'messages' => array()
+		),
+		
+		'ext.datatables.autofill' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/AutoFill/js/dataTables.autoFill.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/AutoFill/css/autoFill.dataTables.css'
+			),
+			'messages' => array()		
+		),
+
+		'ext.datatables.buttons' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/Buttons/js/dataTables.buttons.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/Buttons/css/buttons.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.colreorder' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/ColReorder/js/datatables.colReorder.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/ColReorder/css/colReorder.dataTables.css'
+			),
+			'messages' => array()		
+		),
+				
+		'ext.datatables.fixedcolumns' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/FixedColumns/js/dataTables.fixedColumns.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/FixedColumns/css/fixedColumns.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.fixedheader' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/FixedHeader/js/dataTables.fixedHeader.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/FixedHeader/css/fixedHeader.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.keytable' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/KeyTable/js/dataTables.keyTable.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/KeyTable/css/keyTable.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.responsive' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/Responsive/js/dataTables.responsive.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/Responsive/css/responsive.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.rowreorder' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/RowReorder/js/dataTables.rowReorder.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/RowReorder/css/rowReorder.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		'ext.datatables.scroller' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/Scroller/js/datatables.scroller.js'
+			),
+			'styles' => array(
+				'DataTables-1.10.8/extensions/Scroller/css/scroller.datatables.js'
+			),
+			'messages' => array()		
+		),
+
+		'ext.datatables.select' => array(
+			'scripts' => array(
+				'DataTables-1.10.8/extensions/Select/js/datatables.select.js'
+			),
+			'styles' => array(
+			    'DataTables-1.10.8/extensions/Select/css/select.dataTables.css'
+			),
+			'messages' => array()		
+		),
+		
+		# deprecated but available for backward compatibility
+		# newer projects should use Buttons
+		'ext.datatables.colvis' => array(
+			'scripts' => array(
+				'extras/ColVis/media/js/ColVis.js'
+			),
+			'styles' => array(
+			    'extras/ColVis/media/css/ColVis.css'
+			),
+			'messages' => array()		
+		),
+		# deprecated but available for backward compatibility
+		# newer projects should use Buttons and Select
+		'ext.datatables.tabletools' => array(
+			'scripts' => array(
+				'extras/TableTools/media/js/TableTools.js'
+			),
+			'styles' => array(
+			    'extras/TableTools/media/css/TableTools.css',
+			    'extras/TableTools/media/css/TableTools_JUI.css'
+			),
+			'messages' => array()		
+		)
+	);
+
+
+
+	public static function register( $resourceLoader ) {
+		global $wgExtensionAssetsPath;
+		
+		$localpath = dirname( __FILE__ );
+		$remotepath = "$wgExtensionAssetsPath/DataTables";
+		
+		foreach ( self::$modules as $name => $resources ) {
+			$resourceLoader->register( 
+				$name, new ResourceLoaderFileModule( $resources, $localpath, $remotepath )
+			);
+		}
+		
+		return true;
+	}
+}
---
+++ DataTables/README.txt
@@ -0,0 +1,18 @@
+DataTables mediawiki extension notes
+
+The DataTables MW extension was originally written as a wrapper around the DataTables JQuery plugin library by Daniel Renfro
+
+Starting with version 0.2, updating should be doable by downloading the full datatables distribution and adding it to the DataTables extension directory, and then modifying the version number in DataTables.php
+
+The distribution contains optional css and js files that are not currently used. For example, we load the default js and css files but do not register the ones optimized for BootStrap in Mediawiki.
+
+== Version 0.2 ==
+Restructured to facilitate updating the Datatables library
+* All DataTables distro files are now in a DataTables-version directory
+** the old media directory has been removed
+** legacy extras were kept for backward compatibility, if any.
+* extension set is changed to reflect updated extensions
+** Buttons replaces ColVis
+** Buttons and Select replace TableTools
+** Add Responsive
+** Add RowReorder
